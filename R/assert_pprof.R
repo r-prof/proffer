ef62f7c47dbac2d02d48de11cfa784a4954808cf
wlandau-lilly <will.landau@lilly.com>
2020-02-06 12:21:15 -0500

    Use base functions

--- R/assert_pprof.R
+++ R/assert_pprof.R
@@ -18,7 +18,7 @@ test_pprof <- function(
 ) {
   slow_function <- function() {
     n <- 1e3
-    x <- data.frame(x = rnorm(n), y = rnorm(n))
+    x <- data.frame(x = sample.int(n), y = sample.int(n))
     for (i in seq_len(n)) {
       x[i, ] <- x[i, ] + 1
     }
