57b9ca10f5a2ad46b3ac926ad4564f73fd363bd5
Kirill Müller <krlmlr+r@mailbox.org>
2020-02-25 23:28:15 +0100

    Add test

--- tests/testthat/test-profile.R
+++ tests/testthat/test-profile.R
@@ -15,3 +15,22 @@ test_that("record_rprof() and convert to pprof", {
   samples <- profile::read_pprof(pprof)
   expect_silent(profile::validate_profile(samples))
 })
+
+test_that("arguments to record_rprof()", {
+  rprof <- record_rprof(replicate(1e2, sample.int(1e4)))
+  prof <- profile::read_rprof(rprof)
+  expect_silent(profile::validate_profile(prof))
+
+  rprof <- record_rprof(replicate(1e2, sample.int(1e4)), rprof = rprof, append = TRUE)
+  # Needs fix in {profile}
+  suppressWarnings(prof2 <- profile::read_rprof(rprof))
+  expect_silent(profile::validate_profile(prof2))
+
+  expect_gt(nrow(prof2$samples), nrow(prof$samples))
+
+  expect_error(
+    record_rprof(replicate(1e2, sample.int(1e4)), bogus = argument),
+    "bogus",
+    fixed = TRUE
+  )
+})
