b8bb55fde0f928a85882332f731cc8ff883ce2f0
wlandau-lilly <will.landau@lilly.com>
2020-02-28 14:18:25 -0500

    Plan sitrep functions

--- R/sitrep.R
+++ R/sitrep.R
@@ -5,8 +5,11 @@ pprof_sitrep <- function() {
   sitrep_pprof_path()
   sitrep_pprof_env_new()
   sitrep_pprof_env_old()
-  sitrep_go_bin_path()
+  sitrep_pprof_sys()
   sitrep_go_path()
+  sitrep_go_bin_path()
+  sitrep_go_bin_env()
+  sitrep_go_bin_sys()
   sitrep_graphviz_path()
   sitrep_graphviz_env()
   sitrep_graphviz_sys()
@@ -21,37 +24,7 @@ sitrep_pprof_path <- function() {
   )
 }
 
-sitrep_pprof_env_new <- function() {
-  ifelse(
-    file.exists(pprof_path_env_new()),
-    cli_pprof_env_new_found(),
-    cli_pprof_env_new_missing()
-  )
-}
 
-sitrep_pprof_env_old <- function() {
-  ifelse(
-    file.exists(pprof_path_env_old()),
-    cli_pprof_env_old_found(),
-    cli_pprof_env_old_missing()
-  )
-}
-
-sitrep_go_bin_path <- function() {
-  ifelse(
-    file.exists(pprof_path()),
-    cli_go_bin_path_found(),
-    cli_go_bin_path_missing()
-  )
-}
-
-sitrep_go_path <- function() {
-  ifelse(
-    file.exists(pprof_path()),
-    cli_go_path_found(),
-    cli_go_path_missing()
-  )
-}
 
 cli_pprof_path_found <- function() {
   cli::cli_alert_success("pprof path found. {pprof_path()}")
@@ -59,30 +32,6 @@ cli_pprof_path_found <- function() {
 
 cli_pprof_path_missing <- function() {
   cli::cli_alert_danger("pprof path missing. {pprof_path()}")
-}
-
-cli_pprof_path_found <- function() {
-  cli::cli_alert_success("go binary found. {env_go_bin()}")
-}
-
-cli_pprof_path_missing <- function() {
-  cli::cli_alert_danger("go binary missing. {env_go_bin()}")
-}
-
-cli_go_dir_found <- function() {
-  cli::cli_alert_success("go directory found. {env_go_dir()}")
-}
-
-cli_go_dir_missing <- function() {
-  cli::cli_alert_danger("go directory missing. {env_go_dir()}")
-}
-
-cli_dot_found <- function() {
-
-}
-
-cli_dot_missing <- function() {
-
 }
 
 cli_test_pprof <- function() {
