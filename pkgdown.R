c78c56a49d2c4f1d6492d39c22a332cc2dad88d8
wlandau <will.landau@gmail.com>
2020-09-02 10:53:21 -0400

    Address #26

--- pkgdown.R
+++
@@ -1,9 +0,0 @@
-pkgdown::build_site(preview = FALSE)
-lines <- readLines("docs/index.html")
-index <- grep("og.*/logo.png", lines)
-lines[index] <- gsub(
-  "/logo.png",
-  "https://r-prof.github.io/proffer/reference/figures/logo.png",
-  lines[index]
-)
-writeLines(lines, "docs/index.html")
